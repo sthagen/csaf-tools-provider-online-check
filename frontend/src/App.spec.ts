6728c2aeffb5a00f96cba21c9bf2762023dc454b
Ludwig Reiter <ludwig.reiter@intevation.de>
2026-05-05 12:07:56 +0200

    Add vitests to App.vue

---
+++ frontend/src/App.spec.ts
@@ -0,0 +1,89 @@
+import { beforeEach, describe, expect, test, vi } from 'vitest'
+import { flushPromises, mount } from '@vue/test-utils'
+import App from './App.vue'
+import axios from 'axios'
+
+describe("Testing App...", () => {
+
+    let app: any
+    beforeEach(()=> {
+        app = mount(App)
+    })
+
+    test('messageClass', () => {
+        expect(app.vm.messageClass({type: 0})).toBe('text-green')
+        expect(app.vm.messageClass({type: 1})).toBe('text-orange')
+        expect(app.vm.messageClass({type: 2})).toBe('text-red')
+        expect(app.vm.messageClass({type: -1})).toBe('')
+    }),
+    test('extractMessages', () => {
+        app.vm.extractMessages([{ messages: [{text: "Test1", type: 0}]}])
+        expect(app.vm.messagesList).toStrictEqual([{text: "Test1", type: 0}])
+        app.vm.extractMessages([{messages: undefined}])
+        expect(app.vm.messagesList).toStrictEqual([])
+    }),
+    test('extractMessagesFromResultsChecker with json string', () =>{
+        app.vm.extractMessagesFromResultsChecker(
+            '{ "domains": [{"requirements": [{ "messages": [{"text": "Test1", "type": 0}]}]}]}'
+        )
+        expect(app.vm.messagesList).toStrictEqual([{text: "Test1", type: 0}])
+    }),
+    test('extractMessagesFromResultsChecker with requirements null', () => {
+        app.vm.extractMessagesFromResultsChecker(
+            '{ "domains": [{"requirements": null}]}'
+        )
+        expect(app.vm.messagesList).toBe(null)
+    }),
+    test('extractMessagesFromResultsChecker with onject', () => {
+        app.vm.extractMessagesFromResultsChecker(
+            { "domains": [{"requirements": [{ "messages": [{"text": "Test1", "type": 0}]}]}]}
+        )
+        expect(app.vm.messagesList).toStrictEqual([{text: "Test1", type: 0}])
+    }),
+    test('resultClass', () => {
+        const test_oracle = [
+            ['ERROR', 'alert-danger'],
+            ['UNDEFINED', 'alert-danger'],
+            ['DONE_CHECKER', 'alert-success'],
+            ['CACHED_CHECKER', 'alert-success'],
+            ['DEFAULT', 'alert-info']                    
+        ]
+        for (const pair of test_oracle) {
+            app.vm.result = { 'status': pair[0]}
+            expect(app.vm.resultClass).toBe(pair[1])
+        }
+    }),
+    test('backendUrl and apiDocsUrl', () => {
+        const protocol = window.location.protocol
+        const hostname = window.location.hostname
+        expect(app.vm.backendUrl).toBe(`${protocol}//${hostname}:48090`)
+        expect(app.vm.apiDocsUrl).toBe(`${protocol}//${hostname}:48090/api/docs`)
+    }),
+    test('footerText empty', () => {
+        expect(app.vm.footerText).toBe('')
+    }),
+    test('startScan RUNNING', async () => {
+        vi.spyOn(axios, 'post').mockImplementation(() => { return {data: {status: "RUNNING" }}})
+        app.vm.domain = "Test"
+        app.vm.startScan()
+        await flushPromises()
+        expect(app.vm.loading).toBe(false)
+        expect(app.vm.result?.status).toBe('RUNNING')
+    }),
+    test('startScan CACHED_CHECKER', async () => {
+        vi.spyOn(axios, 'post').mockImplementation(() => { 
+            return {data: {
+                status: "CACHED_CHECKER",
+                results_checker: { "domains": [{"requirements": [{ "messages": [{"text": "Test1", "type": 0}]}]}]}
+            }}
+        })
+        app.vm.domain = "Test"
+        app.vm.startScan()
+        await flushPromises()
+        expect(app.vm.loading).toBe(false)
+        expect(app.vm.result?.status).toBe('CACHED_CHECKER')
+        expect(app.vm.messagesList).toStrictEqual([{text: 'Test1', type: 0}])
+
+    })
+
+})
