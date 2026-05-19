016247d09c46da554bbc65004d11a8d0fb5ccb2c
Ludwig Reiter <ludwig.reiter@intevation.de>
2026-05-19 13:31:02 +0200

    Update scan results: no blocks, different header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -50,13 +50,11 @@
                   <h3 class="alert-heading">Scan found in cache</h3>
                 </div>
 
-                <h4 :class="publisherStatus" class="small-margin-top medium-font-size">CSAF publisher</h4>
-                <MessageLine v-for="item of publisherMessages" :key="item.text" :text="item.text" :type="item.type"></MessageLine>
-
-                <h4 :class="providerStatus" class="small-margin-top medium-font-size">CSAF provider</h4>
-                <MessageLine v-for="item of providerMessages" :key="item.text" :text="item.text" :type="item.type"></MessageLine>
-
-                <h4 :class="trustedProviderStatus" class="small-margin-top medium-font-size">CSAF trusted provider</h4>
+                <h4 :class="trustedProviderStatus" class="small-margin-top medium-font-size">
+                  <span v-if="trustedProviderStatus === 'text-green'">PASSED:</span>
+                  <span v-else>FAILED:</span>
+                  CSAF trusted provider
+                </h4>
                 <MessageLine v-for="item of trustedProviderMessages" :key="item.text" :text="item.text" :type="item.type"></MessageLine>
 
                 <p class="small-margin-top">
@@ -281,32 +279,17 @@ export default defineComponent({
     footerText() {
       return import.meta.env.VITE_FOOTER_TEXT || ''
     },
-    publisherMessages() {
+    trustedProviderMessages() {
       if (this.messagesList) {
+        const trustedProviderMessages = []
+
         // requirements 1 (Valid CSAF document), 2 (Filename), 3 (TLS), 4 (TLP:WHITE)
         // Show all messages
-        return this.filterMessageListByNums([1, 2, 3, 4])
-      }
-      return null
-    },
-    publisherStatus() {
-      if (this.publisherMessages) {
-        return this.publisherMessages.filter((msg: MessageData) => msg.type === 2).length === 0 ? 'text-green' : 'text-red'
-      }
-      return 'text-red'
-    },
-    providerMessages() {
-      if (this.messagesList) {
-        const providerMessages = []
-        providerMessages.push(
-          this.publisherStatus === 'text-green'
-            ? {text: 'Is a valid CSAF publisher', type: 0 }
-            : {text: 'Is not a valid CSAF publisher', type: 2 }
-        )
+        trustedProviderMessages.push(...this.filterMessageListByNums([1, 2, 3, 4]))
 
         // requirements 5 (TLP:AMBER and TLP:RED), 6 (Redirects) and 7 (provider-metadata.json)
         // Show all messages
-        providerMessages.push(...this.filterMessageListByNums([5, 6, 7]))
+        trustedProviderMessages.push(...this.filterMessageListByNums([5, 6, 7]))
 
         // requirements min one of 8 (security.txt), 9 (Well-known URL for provider-metadata.json), 10 (DNS path)
         // One must succed, then show that message, else show all messages
@@ -314,13 +297,13 @@ export default defineComponent({
         const req9Messages = this.filterMessageListByNums([9])
         const req10Messages = this.filterMessageListByNums([10])
         if (req8Messages.length > 0  && req8Messages.filter((msg:MessageData) => msg.type === 2).length === 0) {
-          providerMessages.push(...req8Messages)
+          trustedProviderMessages.push(...req8Messages)
         } else if (req9Messages.length > 0  && req9Messages.filter((msg:MessageData) => msg.type === 2).length === 0) {
-          providerMessages.push(...req9Messages)
+          trustedProviderMessages.push(...req9Messages)
         } else if (req10Messages.length > 0  && req10Messages.filter((msg:MessageData) => msg.type === 2).length === 0) {
-          providerMessages.push(...req10Messages)
+          trustedProviderMessages.push(...req10Messages)
         } else {
-          providerMessages.push(...req8Messages, ...req9Messages, ...req10Messages)
+          trustedProviderMessages.push(...req8Messages, ...req9Messages, ...req10Messages)
         }
 
         // requirements dir based 11 (One folder per year), 12 (index.txt), 13 (changes.csv), 14 (Directory listings)
@@ -330,26 +313,10 @@ export default defineComponent({
         const rolieBaseMessages = this.filterMessageListByNums([15, 16, 17])
         if (rolieBaseMessages.filter((msg:MessageData) => msg.type === 2).length
             <= dirBaseMessages.filter((msg: MessageData) => msg.type === 2).length) {
-          providerMessages.push(...rolieBaseMessages)
+          trustedProviderMessages.push(...rolieBaseMessages)
         } else {
-          providerMessages.push(...dirBaseMessages)
+          trustedProviderMessages.push(...dirBaseMessages)
         }
-        return providerMessages
-      }
-      return null
-    },
-    providerStatus() {
-      if (this.providerMessages) {
-        return this.providerMessages.filter(msg => msg.type === 2).length === 0 ? 'text-green' : 'text-red'
-      }
-      return 'text-red'
-    },
-    trustedProviderMessages() {
-      if (this.messagesList) {
-        const trustedProviderMessages = []
-        trustedProviderMessages.push(
-          this.providerStatus === 'text-green' ? {text: 'Is valid CSAF provider', type: 0 }
-                                              : {text: 'Is not a valid CSAF provider', type: 2})
 
         // requirements 18 (Integrity), 19 (Signatures), 20 (Public OpenPGP Key)
         // Show all messages
