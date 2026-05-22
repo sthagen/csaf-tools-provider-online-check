70be0577804a367ef52cbe96e0f144f60b983554
Jan Malte Behrens <janmtbehrens@gmail.com>
2026-05-22 10:29:51 +0200

    feat: Expanded information for users who input an invalid domain or pmd

--- backend/src/validators/request_validator.py
+++ backend/src/validators/request_validator.py
@@ -2,9 +2,6 @@
 # is valid
 # Also checks if requested domain has a valid cache
 # in database -> handles cache lookup
-
-# Involved in: 4, 17, 18, 21
-
 import re
 
 from ..database.redis import Redis_Controller
@@ -35,7 +32,9 @@ def validate_domain(value: str) -> str:
 
     v = value.strip()
     if not re.match(REQUEST_PATTERN, v):
-        raise ValueError("Invalid domain format")
+        raise ValueError(
+            "Invalid domain/PMD format. Please enter a valid Domain or PMD URL. Domains require a non-zero length extension. PMDs must start with 'https://' and end with '/provider-metadata.json'"
+        )
 
     return v
 
