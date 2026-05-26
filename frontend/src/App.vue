caa765f840ade90840b909b4130b5671738cd570
Sebastian Wagner <swagner@intevation.de>
2026-05-26 10:39:23 +0200

    Revert commit 4e941502c38dc365397140e8402f15018c22eb43
    fixes https://github.com/csaf-tools/provider-online-check/issues/105

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -144,32 +144,6 @@
         </div>
       </div>
 
-      <div v-if="recentScans.length > 0" class="row justify-content-center mt-4">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Recently Scanned</h5>
-              <table class="table table-sm table-hover mb-0">
-                <thead>
-                  <tr>
-                    <th>Domain</th>
-                    <th>Scan Time</th>
-                    <th>Scan Duration</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="scan in recentScans" :key="scan.task_id" style="cursor:pointer" @click="domain = scan.domain">
-                    <td>{{ scan.domain }}</td>
-                    <td>{{ formatTime(scan.end_time) }}</td>
-                    <td>{{ scan.duration }}s</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-
       <div class="row justify-content-center mt-4">
         <div class="col-md-12">
           <div class="card">
@@ -203,14 +177,6 @@ import { defineComponent } from 'vue'
 import MessageLine from './MessageLine.vue'
 import VersionDisplay from './VersionDisplay.vue';
 
-interface RecentScan {
-  task_id: string;
-  domain: string;
-  start_time: number;
-  end_time: number;
-  duration: number;
-}
-
 interface ResultCheckerData {
   domains: { requirements: {num: number, messages: {text: string, type: number}[]}[]}[];
   date: string;
@@ -224,7 +190,6 @@ interface AppData {
   error: any;
   messagesList: null | MessageData[];
   scanTime: null | string;
-  recentScans: RecentScan[];
   version: {
     csaf_checker_version: string;
     csaf_validator_version: string;
@@ -250,7 +215,6 @@ export default defineComponent({
       error: null,
       messagesList: null,
       scanTime: null,
-      recentScans: [],
       version: null
     } as AppData
   },
@@ -258,9 +222,6 @@ export default defineComponent({
     axios
       .get(`${this.backendUrl}/api/information/`)
       .then(response => this.version = response.data)
-    axios
-      .get(`${this.backendUrl}/api/scans`)
-      .then(response => this.recentScans = response.data)
   },
   computed: {
     resultClass() {
@@ -359,7 +320,6 @@ export default defineComponent({
           const parsedResultsChecker = this.parseResultsChecker(this.result.results_checker)
           this.extractMessagesFromResultsChecker(parsedResultsChecker)
           this.setScanTime(parsedResultsChecker)
-          axios.get(`${this.backendUrl}/api/scans`).then(r => this.recentScans = r.data)
         } else {
           this.messagesList = null
           this.scanTime = null
