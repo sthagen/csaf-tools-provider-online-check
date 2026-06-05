070db4047bef5ee434638b0b25caf160b6458a95
Sebastian Wagner <swagner@intevation.de>
2026-06-05 15:52:56 +0200

    feat: compact display for lots of messages
    visually group messages per requirement
    show the name of the requirement
    show a counter of warnings and errors per requirement
    only show 5 messages per requirement by default
    expanding and collapsing on demand
    
    fixes https://github.com/csaf-tools/provider-online-check/issues/131

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -62,7 +62,13 @@
                   <span v-else>FAILED:</span>
                   {{ role }}
                 </h4>
-                <MessageLine v-for="item of trustedProviderMessages" :key="item.text" :text="item.text" :type="item.type"></MessageLine>
+                <MessageGroup
+                  v-for="group of groupedTrustedProviderMessages"
+                  :key="group.num"
+                  :num="group.num"
+                  :description="group.description"
+                  :messages="group.messages"
+                />
 
                 <p class="small-margin-top">
                     <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseAllMessages" role="button" aria-expanded="false" aria-controls="collapseAllMessages"
@@ -193,15 +199,22 @@
 import axios from 'axios'
 import { defineComponent } from 'vue'
 import MessageLine from './MessageLine.vue'
+import MessageGroup from './MessageGroup.vue'
 import VersionDisplay from './VersionDisplay.vue';
 
 interface ResultCheckerData {
   domains: {
-    requirements: {num: number, messages: {text: string, type: number}[]}[],
+    requirements: {num: number, description: string, messages: {text: string, type: number}[]}[],
     passed: boolean;
     role: string;
   }[];
   date: string;
+}
+
+interface RequirementGroup {
+  num: number;
+  description: string;
+  messages: { text: string; type: number }[];
 }
 
 interface AppData {
@@ -213,6 +226,7 @@ interface AppData {
   result: any;
   error: any;
   messagesList: null | MessageData[];
+  requirementGroups: RequirementGroup[];
   scanTime: null | string;
   passed: boolean;
   role: string | null;
@@ -234,7 +248,7 @@ interface MessageData {
 
 export default defineComponent({
   name: 'App',
-  components: { MessageLine, VersionDisplay },
+  components: { MessageLine, MessageGroup, VersionDisplay },
   data() {
     return {
       session_id: '1',
@@ -245,6 +259,7 @@ export default defineComponent({
       result: null,
       error: null,
       messagesList: null,
+      requirementGroups: [],
       scanTime: null,
       passed: false,
       role: null,
@@ -333,6 +348,21 @@ export default defineComponent({
       }
       return null
     },
+    groupedTrustedProviderMessages(): RequirementGroup[] {
+      const flatMessages = this.trustedProviderMessages
+      if (!flatMessages) return []
+      const groupMap = new Map<number, RequirementGroup>()
+      for (const g of this.requirementGroups) {
+        groupMap.set(g.num, { num: g.num, description: g.description, messages: [] })
+      }
+      for (const msg of flatMessages) {
+        const group = groupMap.get(msg.num)
+        if (group) {
+          group.messages.push({ text: msg.text, type: msg.type })
+        }
+      }
+      return [...groupMap.values()].filter(g => g.messages.length > 0)
+    },
     trustedProviderStatus() {
       return this.passed ? 'text-green': 'text-red'
     },
@@ -398,6 +428,7 @@ export default defineComponent({
     },
     clearFields() {
       this.messagesList = null
+      this.requirementGroups = []
       this.scanTime = null
       this.passed = false
     },
@@ -443,10 +474,13 @@ export default defineComponent({
         this.messagesList = null
       }
     },
-    extractMessages(requirements: {num: number, messages: {text: string, type: number}[]}[]) {
+    extractMessages(requirements: {num: number, description: string, messages: {text: string, type: number}[]}[]) {
       this.messagesList = []
+      this.requirementGroups = []
       for (const req of requirements) {
-        for (const msg2 of req.messages ?? []) {
+        const msgs = req.messages ?? []
+        this.requirementGroups.push({ num: req.num, description: req.description ?? '', messages: msgs })
+        for (const msg2 of msgs) {
           this.messagesList.push({type: msg2.type, text: msg2.text, num: req.num })
         }
       }
